91cfa58a0c3307604dc336236260c1d7f1a5cb91
FelipeSEugenio <felipedesiqueirawork@gmail.com>
2026-03-05 15:47:21 -0300

    fix: expose BikeSelector to LWC and improve bikeCatalog UX

--- force-app/main/default/lwc/bikeCatalog/bikeCatalog.js
+++ force-app/main/default/lwc/bikeCatalog/bikeCatalog.js
@@ -2,18 +2,27 @@ import { LightningElement, wire } from 'lwc';
 import getActiveBikes from '@salesforce/apex/BikeSelector.getActiveBikes';
 
 export default class BikeCatalog extends LightningElement {
-
-    bikes;
+    bikes = [];
+    errorMessage;
+    isLoading = true;
 
     @wire(getActiveBikes, { limitSize: 50 })
     wiredBikes({ error, data }) {
+        this.isLoading = false;
 
         if (data) {
             this.bikes = data;
-        }
-
-        if (error) {
+            this.errorMessage = undefined;
+        } else if (error) {
+            this.bikes = [];
+            this.errorMessage = error?.body?.message || 'Erro ao carregar bikes.';
+            // log para debug
+            // eslint-disable-next-line no-console
             console.error(error);
         }
+    }
+
+    get hasBikes() {
+        return this.bikes && this.bikes.length > 0;
     }
 }
