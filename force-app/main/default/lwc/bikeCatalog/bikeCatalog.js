c56050bb5d0379ab828cada6fd9a6e1d88056507
FelipeSEugenio <felipedesiqueirawork@gmail.com>
2026-03-05 15:44:40 -0300

    feat: add bikeCatalog LWC using Apexv2

--- force-app/main/default/lwc/bikeCatalog/bikeCatalog.js
+++ force-app/main/default/lwc/bikeCatalog/bikeCatalog.js
@@ -1,3 +1,19 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import getActiveBikes from '@salesforce/apex/BikeSelector.getActiveBikes';
 
-export default class BikeCatalog extends LightningElement {}
+export default class BikeCatalog extends LightningElement {
+
+    bikes;
+
+    @wire(getActiveBikes, { limitSize: 50 })
+    wiredBikes({ error, data }) {
+
+        if (data) {
+            this.bikes = data;
+        }
+
+        if (error) {
+            console.error(error);
+        }
+    }
+}
